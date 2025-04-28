fff484d504aeab93c2a4c3e3cf9fcf1c38b2ca1e
sidd-bhatt <siddharth11bhatt@gmail.com>
2025-04-28 14:19:38 +0530

    SubArrayWithGivenSum

--- Arrays/src/SubArrayWithGivenSum.java
+++ Arrays/src/SubArrayWithGivenSum.java
@@ -1,2 +1,29 @@
-public class SubArrayWithGivenSum {
+import java.util.Arrays;
+
+public class SubArrayWithGivenSum
+{
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 20, 3, 10, 5};
+        int[] result = (subArrayWithSum(arr, 33));
+        System.out.println("Sub array indecies "+ Arrays.toString(result));
+    }
+
+    public static int[] subArrayWithSum(int[] arr, int targetSum) {
+        int length = arr.length;
+        int sum = 0;
+        int start = 0;
+        //Loop to locate the sum which is equal or greater than the target sum/
+        for(int end=0; end<length; end++) {
+            sum += arr[end];
+            //loop to shift the starting index towards end if sum becomes greater than target sum
+            while(sum > targetSum && start <= end){
+                sum -= arr[start];
+                start++;
+            }
+            if(sum == targetSum){
+                return new int[]{start, end};
+            }
+        }
+        return new int[]{-1};
+    }
 }
